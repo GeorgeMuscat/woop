43245e09965a5bd2913642a644a046843679f543
George <georgemuscat@protonmail.com>
2022-08-28 01:43:38 +1000

    updated server and user

--- src/commands/server.js
+++ src/commands/server.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('server')
-        .setDescription('Replies with server info!'),
+        .setDescription('Replies with server info!')
+        .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nServer Creation Date: ${interaction.guild.createdTimestamp}`);
     },
--- src/commands/user.js
+++ src/commands/user.js
@@ -3,8 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('user')
-        .setDescription('Replies with caller\'s user info!'),
-    async execute(interaction) {
-        await interaction.reply(`Your Username: ${interaction.user.username}\nYour id: ${interaction.user.id}`);
+        .setDescription('Replies with caller\'s user info!')
+        .addUserOption(option => option.setName('target').setDescription('Select a user')),
+        async execute(interaction) {
+            const user = interaction.options.getUser('target')
+        if (user){
+            await interaction.reply(`Username: ${user.username}\nID: ${user.id}`)
+        } else {
+            await interaction.reply(`Your Username: ${interaction.user.username}\nID: ${interaction.user.id}`);
+        }   
     },
 };
