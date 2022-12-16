8cd5eaf95ffbcc98887034093c6a18dfd31b324f
George Muscat <georgemuscat@proton.me>
2022-12-17 06:43:28 +1100

    [feature] made all commands except IP global

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,8 +1,8 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('../config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config;
+const ip = require('./commands/ip');
+require('dotenv').config();
 const token = process.env.TOKEN;
 const guildId = process.env.GUILD_ID;
 const clientId = process.env.CLIENT_ID;
@@ -13,11 +13,17 @@ const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+var ipCommand = null;
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);;
-	commands.push(command.data.toJSON());
+	const command = require(filePath);
+	const data = command.data.toJSON();
+	if (data.name === 'ip') {
+		ipCommand = data;
+		continue;
+	}
+	commands.push(data);
 }
 
 // Construct and prepare an instance of the REST module
@@ -26,17 +32,26 @@ const rest = new REST({ version: '10' }).setToken(token);
 // and deploy your commands!
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${commands.length + (ipCommand !== null ? 1 : 0)} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		if (ipCommand !== null) {
+			await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: [ipCommand] },
+			);
+
+		}
+
+		console.log(`Successfully reloaded ${data.length + (ipCommand !== null ? 1 : 0)} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
+
