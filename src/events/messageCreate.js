30b69e388594f4d8b8b461c118a975be8bcbd1df
George Muscat <georgemuscat@proton.me>
2022-12-18 03:10:51 +1100

    [fix] fixed typo and verified feature

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,7 +3,8 @@ const sku = '318739598899412992';
 module.exports = {
 	name: 'messageCreate',
 	execute(message) {
-		if (messsage.author.id === sku && /gog|\:\(/.test(message.content)) {
+		if (message.author.id === sku && /gog|\:\(/.test(message.content)) {
+			console.log('Sent patpat to sku');
 			message.author.send("https://tenor.com/view/pat-garrys-mod-garrys-mod-physics-fast-intense-gif-26322619");
 		}
 		console.log(`message content: ${message.content}`);
