e9a3a4c4f361e2c9569280614f549c5f4fcbb8fb
George Muscat <georgemuscat@proton.me>
2022-12-18 03:14:51 +1100

    [fix] removed debugging logs

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 require('dotenv').config();
 const token = process.env.TOKEN;
-console.log(token);
 
 // Create a new client instance
 const client = new Client({
@@ -37,7 +36,6 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
-        console.log(command.data.name);
 		client.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
