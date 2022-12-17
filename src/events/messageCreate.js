d426cd568017ab614b358fe02c5586b09f8b9cd4
GeorgeMuscat <GeorgeMuscat@protonmail.com>
2022-12-18 02:39:33 +1100

    [feature] added listening for message from skye

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,11 @@
+const sku = '318739598899412992';
+
 module.exports = {
 	name: 'messageCreate',
 	execute(message) {
+		if (messsage.author.id === sku && /gog|\:\(/.test(message.content)) {
+			message.author.send("https://tenor.com/view/pat-garrys-mod-garrys-mod-physics-fast-intense-gif-26322619");
+		}
 		console.log(`message content: ${message.content}`);
 	},
 };
