13e190f1f0ad5e1c198386ec2536a85da6c4bcb2
George Muscat <georgemuscat@proton.me>
2022-12-18 23:12:42 +1100

    [feature] added cat command

---
+++ src/commands/cat.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+const getUrl = async () => {
+    const res = await axios.get('https://api.thecatapi.com/v1/images/search');
+    let data = res.data;
+    return data[0].url
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('Replies with a random image of a cat!'),
+    async execute(interaction) {
+        await interaction.reply(await getUrl());
+    },
+};
