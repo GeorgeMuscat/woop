e1e4d5ef36149cba83121a33be9e9dd0ef908d70
George Muscat <georgemuscat@proton.me>
2022-12-31 06:54:21 +1100

    [feature] short

--- src/commands/shorten.js
+++ src/commands/shorten.js
@@ -25,6 +25,6 @@ module.exports = {
         } else {
             await interaction.reply('Invalid input.')
         }
-        await interaction.reply(`You short link is: http://woop.li/${postURL(data)}`);
+        await interaction.reply(`You short link is: http://woop.li/${await postURL(data)}`);
     },
 };
