5e0f6a8d4051ffd7e2a7bf4a89ef44fb143ff94c
George Muscat <georgemuscat@proton.me>
2022-12-31 16:28:36 +1100

    [feature] added more error handling

--- src/commands/shorten.js
+++ src/commands/shorten.js
@@ -3,6 +3,11 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const postURL = async (data) => {
     const res = await axios.post('http://woop.li/create', data);
+    if (res.status === 418) {
+        throw new Error('The requested slug is already taken.')
+    } else if (res.status !== 200) {
+        throw new Error('The request could not be fulfilled.');
+    }
     data = res.data;
     return data.slug;
 }
@@ -17,14 +22,21 @@ module.exports = {
         let data = {};
         const url = interaction.options.getString('url');
         const slug = interaction.options.getString('slug');
+
         if (url) {
             data['dest'] = url;
             if (slug) {
                 data['slug'] = slug;
             }
         } else {
-            await interaction.reply('Invalid input.')
+            await interaction.reply('Invalid input.');
         }
-        await interaction.reply(`Your short link is: http://woop.li/${await postURL(data)}`);
+
+        try {
+            const ret = await postURL(data);
+            await interaction.reply(`Your short link is: http://woop.li/${ret}`);
+        } catch (error) {
+            await interaction.reply(error.message);
+        }
     },
 };
